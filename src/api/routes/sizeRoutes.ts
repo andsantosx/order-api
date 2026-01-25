72ae0a49c9b433046b1a0ab1df8c1cb44663ca0b
andsantosx <ardrisonsantos321@gmail.com>
2026-01-24 22:18:29 -0300

    refactor: make sizes read-only - removed POST and DELETE endpoints

--- src/api/routes/sizeRoutes.ts
+++ src/api/routes/sizeRoutes.ts
@@ -1,18 +1,11 @@
 import { Router } from 'express';
 import { SizeController } from '../controllers/SizeController';
-import { validate } from '../middlewares/validate';
-import { createSizeSchema } from '../schemas/sizeSchemas';
-import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = Router();
 const sizeController = new SizeController();
 
-// Public routes
+// Public routes - apenas leitura
 router.get('/', sizeController.getAll.bind(sizeController));
 router.get('/:id', sizeController.getOne.bind(sizeController));
-
-// Admin routes
-router.post('/', authMiddleware, validate(createSizeSchema), sizeController.create.bind(sizeController));
-router.delete('/:id', authMiddleware, sizeController.delete.bind(sizeController));
 
 export default router;
