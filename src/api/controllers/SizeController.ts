e031f66c6f153426c1d6f0c6481402455c4a3308
andsantosx <ardrisonsantos321@gmail.com>
2026-01-24 22:19:44 -0300

    refactor: remove create/delete methods from Size service and controller

--- src/api/controllers/SizeController.ts
+++ src/api/controllers/SizeController.ts
@@ -15,16 +15,4 @@ export class SizeController {
         const size = await this.sizeService.getOne(id as string);
         res.json(size);
     }
-
-    async create(req: Request, res: Response, next: NextFunction) {
-        const { name, type } = req.body;
-        const size = await this.sizeService.create(name, type);
-        res.status(201).json(size);
-    }
-
-    async delete(req: Request, res: Response, next: NextFunction) {
-        const { id } = req.params;
-        const result = await this.sizeService.delete(id as string);
-        res.json(result);
-    }
 }
